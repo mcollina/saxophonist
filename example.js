e23b8c267cbf21a71aba0e1c18e2ea96175d0e37
Matteo Collina <hello@matteocollina.com>
2020-02-12 13:42:46 +0100

    Bumped v2.0.0

--- example.js
+++ example.js
@@ -8,7 +8,7 @@ var count = 0
 console.time('parsing time')
 
 fs.createReadStream(p.join(__dirname, 'wikipedia', '1.xml'))
-  .pipe(saxophonist('page'))
+  .pipe(saxophonist('page', { highWaterMark: 1024 }))
   .on('data', function () {
     count++
   })
